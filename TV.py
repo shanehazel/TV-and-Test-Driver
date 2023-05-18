8870afa49be427c2a017d079f8690b00f9a51ba3
Shane <gupitshanehazel@gmail.com>
2023-05-19 00:20:43 +0800

    pseudocode

---
+++ TV.py
@@ -0,0 +1,17 @@
+# pseudocode
+
+# class for TV
+    # channel int
+    # volumeLevel int
+    # on bool
+
+    # def turn on
+    # def turn off
+    # def getChannel
+    # def setChannel
+    # def getVolume
+    # def setVolume
+    # def channelUp
+    # def channelDown
+    # def volumeUp
+    # def volumeDown
---
+++ Test Driver.py
@@ -0,0 +1,15 @@
+# pseudocode
+
+# import TV
+
+# class for TestTV
+
+    # def argument (turnOn, setChannel, setVolume)
+    # def testTv1
+    # def testTv2
+
+# object tv1
+# call tv1
+
+# object tv2
+# call tv2
