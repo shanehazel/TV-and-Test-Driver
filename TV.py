34c60e4b00d5afacc537b541691fc0fa65c01241
Shane <gupitshanehazel@gmail.com>
2023-05-19 00:27:39 +0800

    def channel, volume leve, on/off

--- TV.py
+++ TV.py
@@ -1,9 +1,15 @@
 # pseudocode
 
 # class for TV
+class TV:
+
+    def __init__(self):
     # channel int
+        self.channel = 1
     # volumeLevel int
+        self.volumeLevel = 1
     # on bool
+        self.on = False
 
     # def turn on
     # def turn off
